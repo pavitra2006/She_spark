c9d6d4120966525e71c743c5e5cf518e4388c4e2
pavitra2006 <147791178+pavitra2006@users.noreply.github.com>
2025-08-01 10:15:09 +0000

    correcting the code

--- src/gemini_client.py
+++ src/gemini_client.py
@@ -1,7 +1,9 @@
 # src/gemini_client.py
 import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 
+load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINIAI_API_KEY")
 if not GEMINI_API_KEY:
     raise ValueError("GEMINIAI_API_KEY environment variable not set. Please add it to your .env file.")
